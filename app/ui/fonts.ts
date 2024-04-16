70a7e03e53e14d06c204997962e5b40cfecd620a
Toorain <maxencewilmes@gmail.com>
2024-04-16 21:33:18 +0200

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+import {NextFont} from "next/dist/compiled/@next/font";
+
+export const inter: NextFont = Inter({ subsets: ['latin'] });
+
+export const lusitana: NextFont = Lusitana({weight: ['400', '700'], subsets: ['latin']})
